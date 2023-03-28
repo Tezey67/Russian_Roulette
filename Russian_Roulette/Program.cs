b25cd5ddab5c14aa73558d7c5c8eb7981431c48b
Tezey67 <83818238+Tezey67@users.noreply.github.com>
2023-03-28 19:10:56 +0300

    Добавьте файлы проекта.

---
+++ Russian_Roulette/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+   // private static void Main(string[] args)
+    
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            RussianRoulette();
+            Console.ReadKey();
+        }
+        static void RussianRoulette()
+        {
+            for (int i = 6; i > 0; i--)
+            {
+                double x = 100 * (double)1 / i;
+                double rand = rnd.Next(0, 99) + rnd.NextDouble();
+                    Console.Write("Жми на курок");
+                    Console.ReadLine();
+                if (x < rand)
+                    Console.WriteLine("Ты выжил");
+                else { Console.WriteLine("'F' Вы умерли"); return; }
+                    Console.ReadLine();
+
+            }
+        }
+   
+}
